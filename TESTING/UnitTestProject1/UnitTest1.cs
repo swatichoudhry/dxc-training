44a46e406171a4792133aae7942ceff54cfd0ea7
swatichoudhry <55695298+swatichoudhry@users.noreply.github.com>
2019-11-22 17:05:44 +0530

    SELENIUM ASSESSMENT-UnitTest1

---
+++ TESTING/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver myD;
+            myD = new ChromeDriver("C:\\selenium jars");
+            //FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\selenium jars", "geckodriver.exe");
+            //service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            //myD = new FirefoxDriver(service);
+            myD.Url = " http://www.youcandealwithit.com/";
+
+            //Thread.Sleep(2000);
+
+            myD.Manage().Window.Maximize();
+            // myD.Manage().Timeouts().ImplicitWait = Timespan.FormSeconds(30);
+
+
+            myD.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(30);
+            Actions act=new Actions(myD);
+            
+            act.MoveToElement(myD.FindElement(By.LinkText("BORROWERS"))).Build().Perform();
+            
+           // Thread.Sleep(2000);
+            myD.FindElement(By.LinkText("Calculators & Resources")).Click();
+            validation(myD.Title, "Calculators & Resources");
+            //Thread.Sleep(2000);
+            myD.FindElement(By.LinkText("Calculators")).Click();
+            validation(myD.Title, "Calculators");
+
+           // Thread.Sleep(2000);
+            myD.FindElement(By.LinkText("Budget Calculator")).Click();
+            validation(myD.Title, "Budget Calculator");
+
+            //Thread.Sleep(2000);
+            myD.FindElement(By.Id("food")).SendKeys("1000");
+            myD.FindElement(By.Id("food")).SendKeys(Keys.Tab);
+            myD.FindElement(By.Id("clothing")).SendKeys("1000");
+            myD.FindElement(By.Id("clothing")).SendKeys(Keys.Tab);
+            myD.FindElement(By.Id("shelter")).SendKeys("1000");
+            myD.FindElement(By.Id("shelter")).SendKeys(Keys.Tab);
+            myD.FindElement(By.Id("household")).SendKeys("1000");
+            myD.FindElement(By.Id("household")).SendKeys(Keys.Tab); 
+            myD.FindElement(By.Id("transportation")).SendKeys("1000");
+            myD.FindElement(By.Id("transportation")).SendKeys(Keys.Tab);
+            string MonthlyPay = "1000";
+            myD.FindElement(By.Id("monthlyPay")).SendKeys(MonthlyPay);
+            myD.FindElement(By.Id("monthlyPay")).SendKeys(Keys.Tab);
+
+            myD.FindElement(By.Id("monthlyOther")).SendKeys("1000");
+
+            //Thread.Sleep(4000);
+            string x = myD.FindElement(By.Id("underOverBudget")).GetAttribute("value");
+            System.Console.WriteLine("Budget is: " + x);
+                
+            if (double.Parse(x)>=double.Parse(MonthlyPay))
+            {
+                System.Console.WriteLine("You are Warren Buffet.");
+
+            }
+            else
+            {
+                System.Console.WriteLine("You are Shashank.");
+
+            }
+            myD.Close();
+
+        }
+
+        public void validation(string s1,string s2)
+        {
+            if (s1.Contains(s2))
+            {
+
+                System.Console.WriteLine("Pass for "+s2);
+            }
+            else
+            {
+                System.Console.WriteLine("Fail for "+ s2);
+            }
+            
+
+        }
+    }
+}
---
+++ TESTING/UnitTestProject1/UnitTest3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest3
+    {
+
+        IWebDriver myD;
+
+        public By oSearch = By.Name("q");
+        public By oSubmit = By.Name("btnK");
+        
+        public void SearchText(string a)
+        {
+            
+            myD.FindElement(oSearch).SendKeys(a);
+
+        }
+        public void SubmitButton()
+        {
+            
+            myD.FindElement(oSubmit).Click();
+        }
+
+
+    }
+
+
+
+    [TestClass]
+    public class Test
+    {
+        public static void Test1()
+        {
+            UnitTest3 ut = new UnitTest3();
+            IWebDriver myD1;
+            myD1 = new ChromeDriver("C:\\selenium jars");
+            myD1.Url = "https://www.google.com/";
+            ut.SearchText("Shashnak");
+            ut.SubmitButton();
+
+        }
+    }
+}
